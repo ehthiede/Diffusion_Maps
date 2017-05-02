c69f5f10bb0ea6bb0c6075b7b983c6e9177444d9
Erik Henning Thiede <thiede@uchicago.edu>
2017-05-02 13:44:25 -0500

    Added verbosity options to diffusion map

--- src/galerkin.py
+++ src/galerkin.py
@@ -46,8 +46,6 @@ def get_committor_dense(evecs,state_A,state_B,complement=None,dt_eff=1.,normaliz
     L_prop = np.dot(u_dot_ind,du)/(N-1)
 
     # Get boundary terms
-    print 'state A check',np.sum(evecs[:,0]*state_A)
-    print 'state B check',np.sum(evecs[:,-1]*state_B)
     L_A = np.dot(np.transpose(evecs)*state_A,evecs)/N
     L_B = np.dot(np.transpose(evecs)*state_B,evecs)/N
     mod_Gen = L_prop+L_A+L_B
@@ -63,6 +61,34 @@ def get_committor_dense(evecs,state_A,state_B,complement=None,dt_eff=1.,normaliz
     g = np.dot(evecs,x)
     return g
     
+def get_ht(evecs,state_A,dt_eff=1.,normalize=False):
+    # Normalize eigenvectors appropriately.
+    evecs = np.array(evecs).astype('float')
+    N = len(evecs)
+    if normalize:
+        evec_norm = np.linalg.norm(evecs,axis=0)
+        evecs*= np.sqrt(N)/evec_norm
+    complement = 1-state_A # Set of data points not in A or B.
+
+    # Get Propagating Term, E[u_j 1_(AUC)^c L u_i]
+    du = np.diff(evecs,axis=0)/dt_eff
+    u_dot_ind = np.transpose(evecs[:-1]) * complement[:-1]
+    L_prop = np.dot(u_dot_ind,du)/(N-1)
+
+    # Get boundary terms
+    L_A = np.dot(np.transpose(evecs)*state_A,evecs)/N
+    mod_Gen = L_prop+L_A
+
+    b =  np.dot(np.transpose(evecs),(1-state_A))/N
+    try:
+        x = spl.solve(mod_Gen,b)
+    except:
+        print mod_Gen
+        print L_A
+        print L_B
+        raise
+    tau = np.dot(evecs,x)
+    return tau
     
     
 def get_tau(data,epsilon,nevecs,alpha=None,beta=None,weights=None,D=1,period=None,nneighb=200,normalize=False):
