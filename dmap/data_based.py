Added new method for getting the stationary distribution, fixed normalization in data_based model

dmap/data_based.py
@@ -14,19 +14,25 @@ import galerkin as gkn
 
 _stat_eval_tol = 10**-5
 
-def get_data_model(basis,traj_edges,delay=1,dt_eff=1):
+def get_data_model(basis,traj_edges,delay=1,dt_eff=1,return_parts=False):
     """
 
     """
     # Orthogonalize basis vectors appropriately
+    basis = dm.clean_basis(basis,traj_edges)
     N,k = np.shape(basis)
+    t_0_indices, t_lag_indices = dm.start_stop_indices(traj_edges,delay)
+    M = len(t_0_indices)
+    reduced_basis = basis[t_0_indices]/np.sqrt(M)
     T_op = gkn.get_transop(basis,traj_edges,delay=delay)
     S = gkn.get_stiffness_mat(basis,traj_edges,delay=delay)
     Sinv = spl.inv(S)
     T_scaled = np.dot(Sinv,np.dot(T_op,Sinv)) # Correct for nonorthogonality
-    T_real = np.dot(basis,np.dot(T_scaled,basis.T))
-    T_sum = np.sum(T_real,axis=1); print np.min(T_sum), np.max(T_sum)
-    return T_real
+
+    if return_parts:
+        return reduced_basis, T_op, S
+    else:
+        return np.dot(reduced_basis,np.dot(T_scaled,reduced_basis.T))
 
 def get_ht(L,stateA):
     """
@@ -39,6 +45,7 @@ def get_ht(L,stateA):
     if isinstance(L_r,sps.spmatrix):
         tau_small = spsl.spsolve(L_r,-np.ones(len(d_locs)))
     else:
+        print np.max(L_r), np.min(L_r)
         tau_small = spl.solve(L_r,-np.ones(len(d_locs)))
     tau = np.zeros(stateA.shape,dtype='float')
     tau[d_locs] = tau_small

dmap/galerkin.py
@@ -111,6 +111,29 @@ def get_committor(basis,g_guess,stateA,stateB,traj_edges,test_set=None,delay=1,e
     delta_g = np.dot(basis,coeffs)
     return g_guess + delta_g
 
+def get_stationary_distrib(basis,traj_edges,test_set=None,delay=1,fix=0):
+    """
+
+    """
+    # Initialize params. 
+    N, k = np.shape(basis)
+    if test_set is None:
+        test_set = basis
+    # Calculate Generator, Stiffness matrix
+    L = get_generator(basis,traj_edges,test_set=test_set,delay=delay,dt_eff=1)
+    # Get stationary distribution
+    nfi = range(0,fix)+range(fix+1,len(L)) #not fixed indices.
+    b = -1*L[fix,nfi]
+    L_submat_transpose = (L[nfi,:][:,nfi]).T
+    rho_notfixed = spl.solve(L_submat_transpose,b)
+    pi = np.ones(k)
+    pi[nfi] = rho_notfixed
+    # Convert to values in realspace.
+    pi_realspace = np.dot(basis,pi)
+    pi_realspace *= np.sign(np.median(pi_realspace))
+    return pi_realspace
+
+
 
 def get_esystem(basis,traj_edges,test_set=None,delay=1):
     """
@@ -120,11 +143,13 @@ def get_esystem(basis,traj_edges,test_set=None,delay=1):
         test_set = basis
     # Calculate Generator, Stiffness matrix
     L = get_generator(basis,traj_edges,test_set=test_set,delay=delay,dt_eff=1)
+#    L = get_transop(basis,traj_edges,test_set=test_set,delay=delay)
     S = get_stiffness_mat(basis,traj_edges,test_set=test_set,delay=delay)
     # Calculate, sort eigensystem
-    evals, evecs_l, evecs_r = spl.eig(L,b=S,left=True,right=True)
+    evals, evecs_l, evecs_r = spl.eig(L,b=S,left=True,right=True,overwrite_a=False,overwrite_b=False)
     idx = evals.argsort()[::-1]
     evals = evals[idx]
+    print np.min(evals), evals[:10], 'eval loc check'
     evecs_l = evecs_l[:,idx]
     evecs_r = evecs_r[:,idx]
     # Expand eigenvectors into real space.